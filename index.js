dc2904b2b43b9d81c473f51e5837fbf59c0e45e5
AriaFallah <aria@fallah.us>
2016-04-18 07:49:55 -0400

    :new: first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "plugins": ["react"],
+  "rules": {
+    "react/display-name": [2, { "ignoreTranspilerName": 2 }],
+    "react/no-danger": 2,
+    "react/no-deprecated": 1,
+    "react/no-did-mount-set-state": 2,
+    "react/no-did-update-set-state": 2,
+    "react/no-direct-mutation-state": 2,
+    "react/no-is-mounted": 2,
+    "react/no-multi-comp": [2, { "ignoreStateless": true }],
+    "react/no-string-refs": 2,
+    "react/no-unknown-property": 2,
+    "react/prefer-es6-class": [2, "always"],
+    "react/prefer-stateless-function": 1,
+    "react/prop-types": 1,
+    "react/react-in-jsx-scope": 2,
+    "react/require-render-return": 2,
+    "react/self-closing-comp": 1,
+    "react/sort-comp": 1,
+    "react/wrap-multilines": [2, { declaration: false, assignment: false, return: true }]
+    "react/jsx-closing-bracket-location": 1,
+    "react/jsx-curly-spacing": [2, "never"],
+    "react/jsx-equals-spacing": [2, "never"],
+    "react/jsx-handler-names": 1,
+    "react/jsx-indent-props": [2, 2],
+    "react/jsx-indent": [2, 2],
+    "react/jsx-key": 1,
+    "react/jsx-no-bind": 2,
+    "react/jsx-no-duplicate-props": 2,
+    "react/jsx-no-literals": 2,
+    "react/jsx-no-undef": 2,
+    "react/jsx-pascal-case": 2,
+    "react/jsx-space-before-closing": [2, "always"],
+    "react/jsx-uses-react": 2,
+    "react/jsx-uses-vars": 2
+  }
+}
